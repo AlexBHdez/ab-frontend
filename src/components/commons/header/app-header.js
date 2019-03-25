1b008a40aae5a6a2b4d509d23790ea780494ca4a
Alex <abhbretones@gmail.com>
2019-03-25 13:38:04 +0100

    UPDATE basic links and nav bg change.

--- src/components/commons/header/app-header.js
+++ src/components/commons/header/app-header.js
@@ -1,7 +1,29 @@
-const navBar = $('.navbar');
-const burgerIcon = $('.navbar-burger');
-const navbarCollapse = $('.navbar-collapse');
+$(window).ready(function() {
+  const navBar = $('.navbar');
+  const burgerIcon = $('.navbar-burger');
+  const navbarCollapse = $('.navbar-collapse');
+  const links = $('app-header a');
+  const hero = $('app-hero');
 
-burgerIcon.click(function() {
-  navBar.toggleClass('bg-primary');
+  burgerIcon.click(function() {
+    navBar.toggleClass('bg-primary');
+  });
+
+  links.click(function() {
+    navbarCollapse.removeClass('show');
+    navBar.toggleClass('bg-primary');
+  });
+
+  
+  $(window).scroll(function() {
+    let heroHeight = hero.height();
+    
+    if($(window).scrollTop() > (heroHeight - 200)) {
+      navBar.addClass('navbar-bg');
+    } else {
+      navBar.removeClass('navbar-bg');
+    }
+  });
 });
+
+
